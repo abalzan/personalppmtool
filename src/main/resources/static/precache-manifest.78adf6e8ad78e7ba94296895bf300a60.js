f0c07c5181c60e9da31f1baf8c6948f55066937c
abalzan <andreibalzan@gmail.com>
2018-12-29 19:13:33 +0000

    putting build application generated in the frontend (npm run build) to static folder

---
+++ src/main/resources/static/precache-manifest.78adf6e8ad78e7ba94296895bf300a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df863df86ea5420a04aa",
+    "url": "/static/js/main.df863df8.chunk.js"
+  },
+  {
+    "revision": "86923d553851038ae31c",
+    "url": "/static/js/1.86923d55.chunk.js"
+  },
+  {
+    "revision": "df863df86ea5420a04aa",
+    "url": "/static/css/main.d3619235.chunk.css"
+  },
+  {
+    "revision": "86923d553851038ae31c",
+    "url": "/static/css/1.83d015be.chunk.css"
+  },
+  {
+    "revision": "8e87a981d520912df073370727bb9ebf",
+    "url": "/index.html"
+  }
+];
